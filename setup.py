f9a3fb1bb1c8ed17b5039d280d2fa51d06695ca1
Konstantinos Bairaktaris <kbairak@transifex.com>
2022-03-17 15:25:48 +0200

    Update setup.py
    Any django version

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name="djsonapi",
       version="0.0.1",
-      install_requires=["Django==3.1.1"],
+      install_requires=["Django"],
       packages=["djsonapi"])
